a61fb4a85283f67d3f0908a028730d2e956265b7
ZapilinEntertainment <mahariusls@yandex.ru>
2016-10-14 19:09:06 +0500

    Add files via upload

---
+++ foot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foot : MonoBehaviour {
+
+	void OnTriggerEnter (Collider c) {
+		if (c.transform.root.tag=="unit"||c.transform.root.tag=="decoration") 
+		{			
+			c.transform.root.SendMessage("Flatten",transform.position,SendMessageOptions.DontRequireReceiver);
+		}
+		if (c.gameObject.layer==8) Global.sm.StepSound(Global.p_controller.prev_step_was_right);
+	}
+}
